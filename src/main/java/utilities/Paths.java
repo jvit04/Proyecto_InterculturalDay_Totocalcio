958476ca25d5e6e7d9a3b559ed515656a838f6ff
José Viteri <jviteri2004@gmail.com>
2026-05-21 11:05:50 -0500

    Actualizar Paths en Version Local:

--- src/main/java/utilities/Paths.java
+++ src/main/java/utilities/Paths.java
@@ -4,14 +4,8 @@ public class Paths {
     public static final String TotocalcioApp = "/mainTotocalcio1.fxml";
     public static final String ImagenIconoApp = "/imagenes/logoTotocalcio.png";
     public static final String UrlBaseDatos= "jdbc:postgresql://localhost:5432/Totocalcio";
-  //Esta parte de código permite colocar las credenciales de la base dentro de las variables de entorno
-    public static final String USER = System.getenv("DB_USER") != null
-            ? System.getenv("DB_USER")
-            : "postgres";
-
-    public static final String PASSWORD = System.getenv("DB_PASSWORD") != null
-            ? System.getenv("DB_PASSWORD")
-            : "123456";
+    public static final String USER= "postgres";
+    public static final String PASSWORD = "Jviteri.5519";
 
 //Usar este comando de git: git config --global core.autocrlf true
     //si sale este mensaje: warning: in the working copy of 'src/main/resources/mainTotocalcio1.fxml', LF will be replaced by CRLF the next time Git touches it
